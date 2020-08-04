fdaf8cea2a6dd7a236f9583437e2b30b0de793a1
SaltyParabola <65374368+SaltyParabola@users.noreply.github.com>
2020-08-04 14:34:05 -0400

    Update budget_google.py

--- budget_google.py
+++ budget_google.py
@@ -1,11 +1,12 @@
-from datetime import datetime
 import gspread
 
-#lets you use Google Docs
-gc = gspread.service_account(<PATH TO FILE WITH CREDENTIALS. NAME THAT FILE service_account>)
+gc = gspread.service_account(<path to google doc credentials>)
 
 
-file_name = 'budget_py' # file name
+file_name = 'budget_py'
+
+file = gc.open(file_name)
+s1 = file.sheet1
 
 class subacct:
 	def __init__(self, name, lst, total):
@@ -29,21 +30,39 @@ class acct:
 			
 
 
-def transac(subacct, amount, desc, acct):
+def transac(subacct, amount, desc, acct, dt):
 	subacct.total += amount
-	date = datetime.now().strftime('%Y-%m-%d')
-	receipt = [date, subacct.name, desc, amount, subacct.total, acct.name, acct.tot()]
+	receipt = [dt, amount, desc, subacct.name, subacct.total, acct.name, acct.tot()]
+	s1.append_row(receipt)
 
-	file = gc.open(file_name).sheet1
-	file.append_row(receipt)
-
-def transfer(subacctfrom, subacctto, amount, desc, acct):
+def transfer(subacctfrom, subacctto, amount, desc, acctfrom, acctto, dt):
 	subacctfrom.total -= amount
 	subacctto.total += amount
-	date = datetime.now().strftime('%Y-%m-%d')
-	receipt1 = [date, subacctfrom.name, desc, -amount, subacctfrom.total, acct.name, acct.tot()]
-	receipt2 = [date, subacctto.name, desc, amount, subacctto.total, acct.name, acct.tot()]
+	receipt1 = [dt, -amount, desc, subacctfrom.name, subacctfrom.total, acctfrom.name, acctfrom.tot()]
+	receipt2 = [dt, amount, desc, subacctto.name, subacctto.total, acctto.name, acctto.tot()]
+	s1.append_row(receipt1)
+	s1.append_row(receipt2)
 
-	file = gc.open(file_name).sheet1
-	file.append_row(receipt1)
-	file.append_row(receipt2)
+
+check = []
+
+travel = subacct('travel', check, 0)
+
+school = subacct('school', check, 0)
+
+necess = subacct('necess', check, 0)
+
+car = subacct('car', check, 0)
+
+subscrip = subacct('subscrip', check, 0)
+
+general = subacct('general', check, 0)
+
+checking = acct('checking', check)
+
+sav = []
+
+save = subacct('save', sav, 0)
+give = subacct('give', sav, 0)
+
+saving = acct('saving', sav)
